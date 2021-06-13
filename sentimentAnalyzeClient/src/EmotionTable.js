9a803d42c6d8cba35015a4734ca03df522b15fb2
vigneshpr2021 <vignesh.prabhu42@nmims.edu.in>
2021-06-13 09:05:14 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -3,20 +3,23 @@ import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
-      return (  
-        <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
-          <table className="table table-bordered">
-            <tbody>
-            {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
-            }
-            </tbody>
-          </table>
-          </div>
-          );
-        }
-    
+        return (
+            <div>
+                <table className="table table-bordered">
+                    <tbody>
+                        {
+                            Object.entries(this.props.emotions).map(function (eventDetails) {
+                                return (<tr>
+                                    <th scope="row">{eventDetails[0]}</th>
+                                    <td>{eventDetails[1]}</td>
+                                </tr>)
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
 }
 export default EmotionTable;
--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,49 +1,100 @@
 const express = require('express');
-const app = new express();
 const dotenv = require('dotenv');
+const app = new express();
+const cors_app = require('cors');
+
 dotenv.config();
 
-function getNLUinstance() {
+function getNULInstance(analyzeParams, text, emotion, res) {
     let api_key = process.env.API_KEY;
     let api_url = process.env.API_URL;
 
     const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
-    const {IamAuthenticator} = require('ibm-watson/auth');
+    const { IamAuthenticator } = require('ibm-watson/auth');
 
     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
         version: '2020-08-01',
         authenticator: new IamAuthenticator({
-            apikey: api_key,
+            apikey: api_key
         }),
-        serviceUrl: api_url,
+        serviceUrl: api_url
     });
-    return naturalLanguageUnderstanding;
+
+    var analyzeParamsNLU = {};
+    if (text) {
+        analyzeParamsNLU = {
+            'text': analyzeParams,
+            'features': {
+                'keywords': {
+                    'emotion': emotion,
+                    'sentiment': !emotion,
+                    'limit': 5,
+                }
+            },
+        };
+    } else {
+        analyzeParamsNLU = {
+            'url': analyzeParams,
+            'features': {
+                'keywords': {
+                    'emotion': emotion,
+                    'sentiment': !emotion,
+                    'limit': 5,
+                },
+            },
+        };
+    }
+
+    naturalLanguageUnderstanding.analyze(analyzeParamsNLU).then(data => {
+        res.status(200);
+        if (data.result.keywords) {
+            var dataFilter = data.result.keywords.sort((a, b) => a.count < b.count ? 1 : -1)[0];
+            console.log(dataFilter);
+            if (emotion) {
+                if (dataFilter.emotion) {
+                    res.send(dataFilter.emotion);
+                } else {
+                    res.send('Without data');
+                }
+
+            } else {
+                if (dataFilter.sentiment.label) {
+                    res.send(dataFilter.sentiment.label);
+                } else {
+                    res.send('Without data');
+                }
+            }
+        } else {
+            res.send('Without data');
+        }
+    }).catch(error => {
+        res.status(500);
+        res.send(error);
+    });
 }
 
 app.use(express.static('client'))
 
-const cors_app = require('cors');
 app.use(cors_app());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('index.html');
-  });
-
-app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
 });
 
-app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+app.get("/url/emotion", (req, res) => {
+    getNULInstance(req.query.url, false, true, res);
 });
 
-app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+app.get("/url/sentiment", (req, res) => {
+    getNULInstance(req.query.url, false, false, res);
 });
 
-app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+app.get("/text/emotion", (req, res) => {
+    getNULInstance(req.query.text, true, true, res);
+});
+
+app.get("/text/sentiment", (req, res) => {
+    getNULInstance(req.query.text, true, false, res);
 });
 
 let server = app.listen(8080, () => {
